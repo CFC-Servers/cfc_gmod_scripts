d4e4323e526e58ee235ca56001a0b1e5407126ae
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-06-10 14:24:12 -0700

    Fix loader

--- lua/cfc_gmod_scripts/init.lua
+++ lua/cfc_gmod_scripts/init.lua
@@ -4,12 +4,16 @@ local function log( name )
     print( "[CFC Gmod Scripts] Loading: ", name )
 end
 
+local topLevel = "cfc_gmod_scripts"
+
 hook.Add( "InitPostEntity", "CFC_GmodScripts_Load", function()
-    local _, dirs = file.Find( "cfc_gmod_scripts/*", "LUA" )
+    local _, dirs = file.Find( topLevel .. "/*", "LUA" )
 
     for i = 1, #dirs do
         local dir = dirs[i]
         log( dir )
-        include( dir .. "/init.lua" )
+
+        local path = string.format( "%s/%s/init.lua", topLevel, dir )
+        include( path )
     end
 end )
