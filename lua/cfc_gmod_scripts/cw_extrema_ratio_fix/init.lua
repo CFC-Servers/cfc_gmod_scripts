e9f6bd36258987183bdc823dbd86cf521cf89a16
Redox <mail@redox.to>
2022-06-11 03:07:40 +0200

    Move initpostentity

--- lua/cfc_gmod_scripts/cw_extrema_ratio_fix/init.lua
+++ lua/cfc_gmod_scripts/cw_extrema_ratio_fix/init.lua
@@ -3,13 +3,15 @@ if SERVER then return end
 
 local className = "cw_extrema_ratio_official"
 
-local drawClumpSpread = weapons.Get( className ).drawClumpSpread
-local stored = weapons.GetStored( className )
+hook.Add( "InitPostEntity", "CFC_GmodScripts_Load", function()
+    local drawClumpSpread = weapons.Get( className ).drawClumpSpread
+    local stored = weapons.GetStored( className )
 
-stored.drawClumpSpread = function( self, ... )
-    if not self.hudScaler then
-        self.hudScaler = ScrH() / 1080
+    stored.drawClumpSpread = function( self, ... )
+        if not self.hudScaler then
+            self.hudScaler = ScrH() / 1080
+        end
+
+        return drawClumpSpread( self, ... )
     end
-
-    return drawClumpSpread( self, ... )
-end
+end )
--- lua/cfc_gmod_scripts/init.lua
+++ lua/cfc_gmod_scripts/init.lua
@@ -6,14 +6,12 @@ end
 
 local topLevel = "cfc_gmod_scripts"
 
-hook.Add( "InitPostEntity", "CFC_GmodScripts_Load", function()
-    local _, dirs = file.Find( topLevel .. "/*", "LUA" )
+local _, dirs = file.Find( topLevel .. "/*", "LUA" )
 
-    for i = 1, #dirs do
-        local dir = dirs[i]
-        log( dir )
+for i = 1, #dirs do
+    local dir = dirs[i]
+    log( dir )
 
-        local path = string.format( "%s/%s/init.lua", topLevel, dir )
-        include( path )
-    end
-end )
+    local path = string.format( "%s/%s/init.lua", topLevel, dir )
+    include( path )
+end
