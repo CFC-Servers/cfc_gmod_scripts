bac57952efa08b9d6919346a0ffeda37fcc96369
plally <pierce@vulpes.dev>
2022-06-17 16:04:57 -0400

    Revert "Add more blacklisted sweps"
    This reverts commit 5a94dca99045daa8f190a67e3837f4548b2faac5.

--- lua/cfc_gmod_scripts/swep-remover/init.lua
+++ lua/cfc_gmod_scripts/swep-remover/init.lua
@@ -5,12 +5,6 @@ local swepsToRemove = {
     weapon_ak47_admin = true, -- Shouldn't be used, admin weapon that does thousands of damage, gives max health and a ton of speed.
     weapon_flashbang = true, -- Errors on use
     weapon_c4 = true, -- Undefusable, makes massive explosion and map wide smoke
-
-    -- M9k admin only weapons
-    m9k_amd65_admin = true,
-    m9k_hk45_admin = true,
-    m9k_l85_admin = true,
-    m9k_mossberg590_admin = true,
 }
 
 hook.Add( "PreRegisterSWEP", "CFC_GmodScripts_RemoveSweps", function( _, class )
