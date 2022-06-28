4e8fa6698772960545f1b3f3db4e9d9ae6a09e1f
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-06-27 18:41:53 -0700

    Add bigchat restriction

---
+++ lua/cfc_gmod_scripts/bigchat-restrictor/init.lua
@@ -0,0 +1,10 @@
+local no = {
+    User = true,
+    Regular = true
+}
+
+hook.Add( "BigChat_CanUse", "CFCGmodScripts_RankRestrict", function( ply )
+    if not IsValid( ply ) then return end
+    local team = team.GetName( ply:Team() )
+    if no[team] then return false end
+end )
