60c3b49c0c486287bfbd9f0596da1434e7f33dd8
Redox <mail@redox.to>
2022-12-25 01:07:46 +0100

    Return earlier

--- lua/cfc_gmod_scripts/mobenix/cl_fix.lua
+++ lua/cfc_gmod_scripts/mobenix/cl_fix.lua
@@ -1,5 +1,6 @@
+if game.GetMap() == "gm_mobenix_v3_final" then return end
+
 hook.Add( "InitPostEntity", "GMS_RemoveMobenixLua", function()
-    if game.GetMap() == "gm_mobenix_v3_final" then return end
     timer.Remove( "mobenix_utrain" )
     timer.Remove( "mobenix_uplayer" )
 end )
