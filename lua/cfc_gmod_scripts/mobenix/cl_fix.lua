f063897e1f06b45cf0985c9a9ae5d74f9025bc9c
Redox <mail@redox.to>
2022-12-25 01:03:22 +0100

    Remove mobenix timers

---
+++ lua/cfc_gmod_scripts/mobenix/cl_fix.lua
@@ -0,0 +1,5 @@
+hook.Add( "InitPostEntity", "GMS_RemoveMobenixLua", function()
+    if game.GetMap() == "gm_mobenix_v3_final" then return end
+    timer.Remove( "mobenix_utrain" )
+    timer.Remove( "mobenix_uplayer" )
+end )
