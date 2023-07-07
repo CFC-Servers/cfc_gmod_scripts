f94595e2cee6123dadb75df2ce9dc53ae424d0c4
Redox <mail@redox.to>
2023-07-07 15:41:15 +0200

    Fix realm inclusions (#22)
    * Fix ugly print
    
    * Fix realm inclusion logic

--- lua/cfc_gmod_scripts/disablebloom/cl_disablebloom.lua
+++ lua/cfc_gmod_scripts/disablebloom/cl_disablebloom.lua
@@ -1,2 +1,2 @@
 RunConsoleCommand( "mat_disable_bloom", 1 )
-print( "[CFC_GmodScripts] Enabled mat_disable_bloom convar to make bloom maps look better" )
+print( "[CFC Gmod Scripts] Enabled mat_disable_bloom convar to make bloom maps look better" )
