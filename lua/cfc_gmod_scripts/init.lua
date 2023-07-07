92a463d913bd0edde77e1f7e146d89f96d765508
Redox <mail@redox.to>
2023-07-07 02:54:28 +0200

    Fix realm inclusion logic

--- lua/cfc_gmod_scripts/init.lua
+++ lua/cfc_gmod_scripts/init.lua
@@ -2,28 +2,56 @@ AddCSLuaFile()
 
 local topLevel = "cfc_gmod_scripts"
 
+local SERVER = SERVER
+local CLIENT = CLIENT
+
 local function log( name )
     print( "[CFC Gmod Scripts] Loading: ", name )
 end
 
 -- ChatGPT says this is the best way to do this
-local function realmInclude( prefix, path )
-    local is_cl, is_sv = prefix == "cl_", prefix == "sv_"
+local function realmInclude( path, isCL, isSV, isSH )
+    if SERVER and ( isCL or isSH ) then
+        AddCSLuaFile( path )
+    end
 
-    if is_cl or not is_sv then AddCSLuaFile( path ) end
-    if is_sv == SERVER or is_cl then return include( path ) end
+    if CLIENT and ( isCL or isSH ) then
+        include( path )
+    end
+
+    if SERVER and ( isSV or isSH ) then
+        include( path )
+    end
 end
 
 local _, dirs = file.Find( topLevel .. "/*", "LUA" )
 
 for _, dir in ipairs( dirs ) do
-    log( dir )
     local files = file.Find( topLevel .. "/" .. dir .. "/*", "LUA" )
+    local isServer, isClient = false, false
 
     for _, fil in ipairs( files ) do
         local prefix = string.sub( fil, 1, 3 )
         local path = dir .. "/" .. fil
 
-        realmInclude( prefix, path )
+        local isCL, isSV, isSH = prefix == "cl_", prefix == "sv_", prefix == "sh_"
+
+        if isCL or isSH then
+            isClient = true
+        end
+
+        if isSV or isSH then
+            isServer = true
+        end
+
+        realmInclude( path, isCL, isSV, isSH )
+    end
+
+    if CLIENT and isClient then
+        log( dir )
+    end
+
+    if SERVER and isServer then
+        log( dir )
     end
 end
