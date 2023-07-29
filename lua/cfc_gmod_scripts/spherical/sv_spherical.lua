79411c78940286b6e4e51e89f4f2b9a8647630a7
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-07-28 20:46:16 -0700

    Add print

--- lua/cfc_gmod_scripts/spherical/sv_spherical.lua
+++ lua/cfc_gmod_scripts/spherical/sv_spherical.lua
@@ -3,13 +3,13 @@ local function hasCustomCollisions( ent )
     return bit.band( solidFlags, bit.bor( FSOLID_CUSTOMRAYTEST, FSOLID_CUSTOMBOXTEST ) ) == bit.bor( FSOLID_CUSTOMRAYTEST, FSOLID_CUSTOMBOXTEST )
 end
 
-
 local EntityMeta = FindMetaTable( "Entity" )
 
 EntityMeta._GMS_PhysicsInitSphere = EntityMeta._GMS_PhysicsInitSphere or EntityMeta.PhysicsInitSphere
 
 function EntityMeta:PhysicsInitSphere( ... )
     if hasCustomCollisions( self ) then
+        print( "[CFC_GmodScripts] [WARN] Attempted to call PhysicsInitSphere on an entity with custom collisions. Ignoring." )
         self:EmitSound( "buttons/button10.wav" )
         return
     end
