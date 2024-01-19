4fdeea6ae4608adec180612141639bdc03691503
Brandon Sturgeon <brandon@brandonsturgeon.com>
2024-01-19 15:45:15 -0800

    simplification

--- lua/cfc_gmod_scripts/player_spawn_underground/sv_init.lua
+++ lua/cfc_gmod_scripts/player_spawn_underground/sv_init.lua
@@ -1,88 +1,54 @@
 require( "playerload" )
 
-local function Deque()
-    local Data = {}
-    local Queue = {}
-
-    local head = 1
-    local tail = 0
-    local size = 0
-
-    function Queue.Push( value )
-        tail = tail + 1
-        Data[tail] = value
-
-        size = size + 1
-    end
-
-    function Queue.Pop()
-        if head > tail then return nil end
-
-        local value = Data[head]
-        Data[head] = nil
-        head = head + 1
-        size = size - 1
-
-        return value
-    end
-
-    function Queue.IsEmpty()
-        return size == 0
-    end
-
-    function Queue.GetSize()
-        return size
-    end
-
-    return Queue
-end
-
-
 local enable = CreateConVar( "cfc_gmodscripts_spawnin_lag_fixer", "1", FCVAR_ARCHIVE, "Spawn players underground to combat lag" )
 
-local plys = {}
-local plyCount = 0
-timer.Create( "SpawnInLagFixer", 1, 0, function()
-    plys = player.GetAll()
-    plyCount = #plys
-end )
-
-local queue = Deque()
-local queue_Push = queue.Push
-local queue_Pop = queue.Pop
-local queue_IsEmpty = queue.IsEmpty
-
+local table_remove = table.remove
+local table_insert = table.insert
 local entMeta = FindMetaTable( "Entity" )
 local Entity_IsValid = entMeta.IsValid
-do
-    local SetPreventTransmit = entMeta.SetPreventTransmit
+local SetPreventTransmit = entMeta.SetPreventTransmit
 
-    hook.Add( "Think", "SpawnInLagFixer", function()
-        if queue_IsEmpty() then return end
+--- Active ent queues for players that have loaded in
+local queues = {}
+--- Player queues that need to be processed when they fully load
+local pending = {}
+--- Entities that were spawned by players
+local trackedEnts = {}
 
+-- Main queue processing loop. Processes N items from each queue
+hook.Add( "Think", "SpawnInLagFixer", function()
+    local queueCount = #queues
+
+    for q = 1, queueCount do
+        local struct = pending[q]
+
+        local ply = struct[1]
+        local queue = struct[2]
+
+        -- Process 10 items from each player's queue
         for i = 1, 10 do
-            local struct = queue_Pop()
-            if not struct then return end
+            local ent = table_remove( queue )
+            if not ent then break end
 
-            local ply = struct[1]
-            local ent = struct[2]
-
-            if ent:IsValid() then
+            if Entity_IsValid( ent ) then
                 SetPreventTransmit( ent, ply, false )
             else
+                -- Always do at least 10 entities for each player if we can
                 i = i - 1
             end
         end
-    end )
-end
+    end
+end )
 
-local trackedEnts = {}
-local table_insert = table.insert
 
+-- On initial spawn, prevent all ents from being transmitted to them
+-- Then track the ents that we stopped transmitting to them
 hook.Add( "PlayerInitialSpawn", "SpawnInLagFixer", function( ply )
     if not enable:GetBool() then return end
 
+    -- While we're looping the tracked ents anyway, we also re-build the list to remove invalid ents
     local newEnts = {}
+    local plyPending = {}
 
     local entCount = #trackedEnts
     for i = 1, entCount do
@@ -90,56 +56,34 @@ hook.Add( "PlayerInitialSpawn", "SpawnInLagFixer", function( ply )
 
         if Entity_IsValid( ent ) then
             table_insert( newEnts, ent )
+            table_insert( plyPending, ent )
+
+            -- TODO: This might need to be queued or something, it'll be laggy
             SetPreventTransmit( ent, ply, true )
         end
     end
+
+    -- These are the ents that we stopped transmitting to them
+    -- When they fully load, we will start transmitting them in batches
+    pending[ply] = plyPending
+
+    -- "passive" validity insurance
+    trackedEnts = newEnts
 end )
 
 
-local pendingEnts = {}
-
-gameevent.Listen( "player_connect" )
-hook.Add( "player_connect", "SpawnInLagFixer", function( data )
-    if not enable:GetBool() then return end
-    local ply = Player( data.userid )
-    pendingEnts[ply] = {}
-end )
-
+--- They fully loaded, now we can queue up their pending ents
 hook.Add( "PlayerFullLoad", "SpawnInLagFixer", function( ply )
     if not enable:GetBool() then return end
 
-    local pending = pendingEnts[ply]
-    local count = #pending
-
-    for i = 1, count do
-        local struct = pending[i]
-        queue_Push( struct )
-    end
-
-    pendingEnts[ply] = nil
+    table_insert( queues, { ply, pending[ply] } )
+    pending[ply] = nil
 end )
 
 local function handleNewEnt( _, model, ent )
     if not enable:GetBool() then return end
     if not ent then ent = model end
-
-    for i = 1, plyCount do
-        local ply = plys[i]
-
-        local struct = { ply, ent }
-        local pending = pendingEnts[ply]
-
-        if pending then
-            -- If they're still loading in, we don't want to start processing their queue yet
-            table_insert( pending, struct )
-        else
-            -- Otherwise, we can just add it to the queue
-            queue_Push( struct )
-        end
-
-        SetPreventTransmit( ent, ply, true )
-        table_insert( trackedEnts, ent )
-    end
+    table_insert( trackedEnts, ent )
 end
 
 hook.Add( "PlayerSpawnedProp", "SpawnInLagFixer", handleNewEnt )
