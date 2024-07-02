5039ef29ef8519eace50392169f2823bfae7bf80
Redox <mail@redox.to>
2024-07-02 15:05:24 +0200

    Improve anti join spam (#44)
    * Improve anti join spam
    
    * Update lua/cfc_gmod_scripts/anti_joinspam/sv_anti_joinspam.lua
    
    Co-authored-by: Brandon Sturgeon <brandon@brandonsturgeon.com>
    
    ---------
    
    Co-authored-by: Brandon Sturgeon <brandon@brandonsturgeon.com>

--- lua/cfc_gmod_scripts/anti_joinspam/sv_anti_joinspam.lua
+++ lua/cfc_gmod_scripts/anti_joinspam/sv_anti_joinspam.lua
@@ -4,7 +4,11 @@ local banDuration = 3
 hook.Add( "CheckPassword", "GMS_AntiJoinSpam", function( steamid64, ip, _, _, name )
     connections[steamid64] = ( connections[steamid64] or 0 ) + 1
 
-    if connections[steamid64] >= 8 then
+    timer.Create( "GMS_ConnectCooldown_" .. steamid64, 5, 1, function()
+        connections[steamid64] = nil
+    end )
+
+    if connections[steamid64] >= 5 then
         ip = string.Explode( ":", ip )[1]
         RunConsoleCommand( "addip", banDuration, ip )
         RunConsoleCommand( "writeip" )
@@ -13,16 +17,5 @@ hook.Add( "CheckPassword", "GMS_AntiJoinSpam", function( steamid64, ip, _, _, na
         print( "GMS Anti join spam: ", name .. " " .. steamid .. " tried to spam join the server." )
 
         return false, "Too many connection attempts"
-    end
-end )
-
-timer.Create( "GMS_AntiJoinSpam", 5, 0, function()
-    for steamid64, count in pairs( connections ) do
-        local newval = math.max( count - 1, 0 )
-        if newval == 0 then
-            connections[steamid64] = nil
-        else
-            connections[steamid64] = newval
-        end
     end
 end )
