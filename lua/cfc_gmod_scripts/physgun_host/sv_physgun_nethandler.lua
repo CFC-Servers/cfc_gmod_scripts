ad9c088a62aa5bd92da956568e6fd43fc12afdf0
Redox <mail@redox.to>
2025-05-10 22:09:18 +0200

    Remove physgun_host detour

--- lua/cfc_gmod_scripts/physgun_host/sv_physgun_nethandler.lua
+++
@@ -1,9 +0,0 @@
--- Makes the physgun net detour properly log errors, original uses xpcall + print.
-if not PhysgunPostCallNetIncomming then return end
-
-local ent = Entity
-function PhysgunPostCallNetIncomming( len, entIndex )
-    ProtectedCall( function()
-        net.Incoming( len, ent( entIndex ) )
-    end )
-end
